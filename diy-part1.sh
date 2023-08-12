cbc123efee696d8c94c642db73c062f7e1f9a770
sdhz151 <yanghui140@gmail.com>
2023-08-12 20:13:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,6 +22,8 @@ svn co https://github.com/kenzok8/small-package/trunk/redsocks2 package/diy/reds
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/diy/luci-app-openclash
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns package/diy/luci-app-smartdns
 svn co https://github.com/kenzok8/small-package/trunk/smartdns package/diy/smartdns
+svn co https://github.com/kenzok8/small-package/trunk/luci-app-mosdns package/diy/luci-app-mosdns
+svn co https://github.com/kenzok8/small-package/trunk/mosdns package/diy/mosdns
 # svn co https://github.com/kenzok8/small-package/trunk/ipt2socks package/diy/ipt2socks
 # svn co https://github.com/kenzok8/small-package/trunk/microsocks package/diy/microsocks
 # svn co https://github.com/kenzok8/small-package/trunk/dns2socks package/diy/dns2socks
