1cb596cfff2bf68bf8ee5ced9722d3422a93b5a8
sdhz151 <yanghui140@gmail.com>
2023-11-07 20:25:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://githttps://github.com/sdhz151/luci-app-mosdns/trunk/luci-app-mosdnshub.com/fw876/helloworld' >>feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
@@ -23,9 +23,9 @@ svn co https://github.com/kenzok8/small-package/trunk/redsocks2 package/diy/reds
 # svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/diy/luci-app-openclash
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns package/diy/luci-app-smartdns
 svn co https://github.com/kenzok8/small-package/trunk/smartdns package/diy/smartdns
-svn co https://github.com/sdhz151/luci-app-mosdns/trunk/luci-app-mosdns package/diy/luci-app-mosdns
-svn co https://github.com/sdhz151/luci-app-mosdns/trunk/mosdns package/diy/mosdns
-svn co https://github.com/sdhz151/luci-app-mosdns/trunk/v2dat package/diy/v2dat
+svn co https://github.com/sbwml/luci-app-mosdns/trunk/luci-app-mosdns package/diy/luci-app-mosdns
+svn co https://github.com/sbwml/luci-app-mosdns/trunk/mosdns package/diy/mosdns
+svn co https://github.com/sbwml/luci-app-mosdns/trunk/v2dat package/diy/v2dat
 # svn co https://github.com/kenzok8/small-package/trunk/ipt2socks package/diy/ipt2socks
 # svn co https://github.com/kenzok8/small-package/trunk/microsocks package/diy/microsocks
 # svn co https://github.com/kenzok8/small-package/trunk/dns2socks package/diy/dns2socks
